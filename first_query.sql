8ee65b6b26bf99b89a5f086e6a33679f8e153ede
Domino Valdano <dvaldano@pivotal.io>
2019-10-31 18:25:22 -0700

    update from OSX

--- first_query.sql
+++ first_query.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS normalized_out;
---CREATE TABLE normalized_out AS
---    SELECT madlib.array_scalar_mult( x, 1.0::REAL) FROM cifar_demo_train;
+CREATE TABLE normalized_out AS
+    SELECT madlib.array_scalar_mult( x, 1.0::REAL) FROM cifar_demo_train;
 
-CREATE TABLE normalized_out_rows AS
+DROP TABLE IF EXISTS normalized_out_rows; CREATE TABLE normalized_out_rows AS
     SELECT row_number() over() % 6 as buffer_id FROM cifar_demo_train;
     
 --CREATE TABLE normalized_out_randomized AS SELECT * FROM normalized_out ORDER BY RANDOM();
