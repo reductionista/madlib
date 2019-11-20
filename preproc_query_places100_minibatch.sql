7833f3022ccfa7e382560618cb281eedf31b84ab
Domino Valdano <dvaldano@pivotal.io>
2019-11-20 03:22:50 +0000

    Switch to / with GroupAgg

--- preproc_query_places100_minibatch.sql
+++ preproc_query_places100_minibatch.sql
@@ -1,5 +1,8 @@
 --    DROP TABLE IF EXISTS places100_train_gpdbagg_out;
-EXPLAIN    CREATE TABLE places100_train_gpdbagg_out AS
+SET optimizer=off;
+--SET gp_enable_multiphase_agg=off;  -- uncomment for single phase HashAgg (only affects Planner)
+SET enable_hashagg=off;  -- uncomment for GroupAgg (only affects Planner)
+           CREATE TABLE places100_train_gpdbagg_out AS
             SELECT __dist_key__ ,
                    madlib.convert_array_to_bytea(independent_var) AS independent_var,
                    madlib.convert_array_to_bytea(dependent_var) AS dependent_var,
@@ -13,7 +16,8 @@ EXPLAIN    CREATE TABLE places100_train_gpdbagg_out AS
                         ARRAY[madlib_places100_normalized.x_norm::REAL[]]) AS independent_var,
                     madlib.gpdb_agg_array_concat(
                         ARRAY[madlib_places100_normalized.y]) AS dependent_var,
-                    (madlib_places100_normalized.row_id%1290.0)::smallint AS buffer_id,
+                    floor(madlib_places100_normalized.row_id/381)::smallint AS buffer_id,
+                    -- shouldn't need floor as long as row_id is BIGINT.  We do need if it's NUMERIC
                     count(*) AS count
                 FROM madlib_places100_normalized
                 GROUP BY buffer_id
