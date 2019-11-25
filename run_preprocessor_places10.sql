433ecdf0fbdf1ae8d793528d44a8dcd7a9c36dc2
Domino Valdano <dvaldano@pivotal.io>
2019-11-25 23:36:40 +0000

    Add half & third buffer sizes for places10, places100

--- run_preprocessor_places10.sql
+++ run_preprocessor_places10.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS places10_train_perf_output, places10_train_perf_output_summary;
 SET enable_hashagg=off;
 SET optimizer=off;
-SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x');
+SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x');  -- default buf size 382
+SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x', 194); -- half buf size
+SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x', 129); -- 1/3 buf size
