3587f769f67d8541ffb1a6c991ae773556ac13b3
Domino Valdano <dvaldano@pivotal.io>
2019-11-26 19:31:51 +0000

    .

--- run_preprocessor_places10.sql
+++ run_preprocessor_places10.sql
@@ -1,6 +1,8 @@
-DROP TABLE IF EXISTS places10_train_perf_output, places10_train_perf_output_summary;
 SET enable_hashagg=off;
 SET optimizer=off;
-SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x');  -- default buf size 382
-SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x', 194); -- half buf size
+--DROP TABLE IF EXISTS places10_train_perf_output, places10_train_perf_output_summary;
+--SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x');  -- default buf size 382
+--DROP TABLE IF EXISTS places10_train_perf_output, places10_train_perf_output_summary;
+--SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x', 194); -- half buf size
+DROP TABLE IF EXISTS places10_train_perf_output, places10_train_perf_output_summary;
 SELECT madlib.training_preprocessor_dl('places10_train', 'places10_train_perf_output', 'y', 'x', 129); -- 1/3 buf size
