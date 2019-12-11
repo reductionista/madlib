9237ef806c8b4dc922a312acb4ea8f209ce9b49c
Domino Valdano <dvaldano@pivotal.io>
2019-12-11 01:13:21 +0000

    Running in 16s now (and with correct output, was not distributed properly before)

--- new_nosort_query.sql
+++ new_nosort_query.sql
@@ -1,21 +1,16 @@
 DROP TABLE IF EXISTS madlib_perm_normalized;
-CREATE TABLE madlib_perm_normalized AS
-            SELECT x::REAL[] AS x_norm,
-                ARRAY[(y) IS NOT DISTINCT FROM 0, (y) IS NOT DISTINCT FROM 1, (y) IS NOT DISTINCT FROM 2, (y) IS NOT DISTINCT FROM 3, (y) IS NOT DISTINCT FROM 4, (y) IS NOT DISTINCT FROM 5, (y) IS NOT DISTINCT FROM 6, (y) IS NOT DISTINCT FROM 7, (y) IS NOT DISTINCT FROM 8, (y) IS NOT DISTINCT FROM 9]::INTEGER[]::SMALLINT[] AS y
-            FROM cifar_demo_train ORDER BY RANDOM()
-                DISTRIBUTED RANDOMLY;
-
-DROP TABLE IF EXISTS madlib_perm_normalized_redist;
-CREATE TABLE madlib_perm_normalized_redist AS SELECT n.x_norm, n.y,
-        d.__dist_key__ AS row_dist_key,
-        ((ROW_NUMBER() OVER(PARTITION BY d.__dist_key__)) - 1)::SMALLINT as slot_id FROM
-                madlib_perm_normalized AS n JOIN perm_dist_key d
-                    ON n.gp_segment_id = d.gp_segment_id
-                    DISTRIBUTED BY (row_dist_key);
+EXPLAIN CREATE TABLE madlib_perm_normalized AS
+            SELECT id,
+            d.__dist_key__ AS row_dist_key,
+            x::REAL[] AS x_norm,
+                ARRAY[(y) IS NOT DISTINCT FROM 0, (y) IS NOT DISTINCT FROM 1, (y) IS NOT DISTINCT FROM 2, (y) IS NOT DISTINCT FROM 3, (y) IS NOT DISTINCT FROM 4, (y) IS NOT DISTINCT FROM 5, (y) IS NOT DISTINCT FROM 6, (y) IS NOT DISTINCT FROM 7, (y) IS NOT DISTINCT FROM 8, (y) IS NOT DISTINCT FROM 9]::INTEGER[]::SMALLINT[] AS y,
+            ((ROW_NUMBER() OVER(PARTITION BY d.__dist_key__ ORDER BY RANDOM())) - 1)::INTEGER as slot_id
+            FROM cifar_demo_train JOIN perm_dist_key AS d ON id % 20 = d.gp_segment_id
+                DISTRIBUTED BY (row_dist_key);
 
 DROP TABLE IF EXISTS madlib_perm_normalized_redist_buffer_id;
 CREATE TABLE madlib_perm_normalized_redist_buffer_id AS
-    SELECT row_dist_key, x_norm, y, ((slot_id * 20 + gp_segment_id) / 2500)::SMALLINT AS buffer_id
+    SELECT row_dist_key, x_norm, y, ((slot_id * 20 + gp_segment_id)::INTEGER / 2500) AS buffer_id
         FROM madlib_perm_normalized_redist ORDER BY buffer_id
             DISTRIBUTED BY (row_dist_key);
 
