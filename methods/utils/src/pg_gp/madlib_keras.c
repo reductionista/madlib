9520c9b09fab366d78e431677e31c438b440a128
Domino Valdano <dvaldano@pivotal.io>
2020-04-15 15:58:30 -0700

    Working:  passes array of BIGINT to python, and receives result back.

--- methods/utils/src/pg_gp/madlib_keras.c
+++ methods/utils/src/pg_gp/madlib_keras.c
@@ -1,6 +1,7 @@
 #include <Python.h>
 #include <postgres.h>
 #include <fmgr.h>
+#include <utils/array.h>
 #include <utils/builtins.h>
 
 #ifndef NO_PG_MODULE_MAGIC
@@ -17,26 +18,65 @@ PG_FUNCTION_INFO_V1(dlc_madlib_keras_fit);
  */
 Datum
 dlc_madlib_keras_fit(PG_FUNCTION_ARGS) {
-    Datum   ydata           = PG_GETARG_DATUM(0);
-    Oid     ydata_typ       = get_fn_expr_argtype(fcinfo->flinfo, 0);
-    Datum   xdata           = PG_GETARG_DATUM(1);
-    Oid     xdata_typ       = get_fn_expr_argtype(fcinfo->flinfo, 1);
+    ArrayType *pg_ydata           = PG_GETARG_ARRAYTYPE_P(0);
+    Oid     ydata_typ          = get_fn_expr_argtype(fcinfo->flinfo, 0);
+    ArrayType *pg_xdata           = PG_GETARG_ARRAYTYPE_P(1);
+    Oid     xdata_typ          = get_fn_expr_argtype(fcinfo->flinfo, 1);
+	PyObject *return_val;
     uint32_t rv = -1;
     Datum retval;
+	PyObject *result;
+//	PyObject* main = PyImport_AddModule("__main__");
+//	PyObject* globalDictionary = PyModule_GetDict(main);
+	PyObject* localDictionary = PyDict_New();
+	PyObject* val;
+	ArrayType *a;
+	PyObject *py_xlist;
+	long *c_xdata = NULL;
 
-    uint32_t b = Py_IsInitialized();
-    if (b) {
-        elog(WARNING, "Yup, python is aleady initialized.\n");
-    } else {
-        elog(WARNING, "Let's go ahead an initialize this...\n");
+	int x_ndim = ARR_NDIM(pg_xdata);
+	int *x_dims = ARR_DIMS(pg_xdata);
+	int x_items = ArrayGetNItems(x_ndim, x_dims);
+	c_xdata = (long *)ARR_DATA_PTR(pg_xdata);
+
+	elog(WARNING, "xdata_type = %d", xdata_typ);
+
+    if (!Py_IsInitialized()) {
         Py_Initialize();
     }
 
-    elog(WARNING, "Running: import keras...");
-    rv = PyRun_SimpleString("import keras");
-    elog(WARNING, "Python call returned with error code %d", rv);
+	py_xlist = PyList_New(x_items);
+	for (int i=0; i < x_items; i++) {
+//		elog(WARNING, "... appending %ld to py_xlist", c_xdata[i]);
+		PyList_SetItem(py_xlist, i, PyInt_FromLong(c_xdata[i]));
+	}
 
-    retval = Int32GetDatum(rv);
+    elog(WARNING, "Calling PyRun_String...");
+
+//	py_xlist = Py_BuildValue("[iiii]", 100,101,102,103);
+
+	const char* pythonScript = "result = 5 * xdata\n";
+//	const char* pythonScript = "result = 5 * [1,2,3]\n";
+	PyDict_SetItemString(localDictionary, "xdata", py_xlist);
+	PyRun_String(pythonScript, Py_file_input, localDictionary, localDictionary);
+    elog(WARNING, "Returned from PyRun_String.");
+	result = PyDict_GetItemString(localDictionary, "result");
+
+	for (Py_ssize_t i=0; i < PyList_Size(result); i++) {
+		val = PyList_GetItem(result, i);
+		elog(WARNING, "result[%ld] = %ld", i, PyInt_AsLong(val));
+	}
+	
+//	if (return_val == NULL) {
+//		elog(ERROR, "Exception in PyRun_String");
+//	} else {
+//		rv = return_val.getInt();
+		rv = 0;
+//	}
+	elog(WARNING, "Size of returned list: %ld", PyList_Size(result));
+	Py_Finalize();
+
+    retval = Int32GetDatum(0);
 
     PG_RETURN_INT32(retval);
 }
