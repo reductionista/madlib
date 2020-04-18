041ebcfb620b5969f3d7325601924ebb18566f99
Nikhil Kak <nkak@pivotal.io>
2020-04-18 01:42:53 +0000

    create UDA

--- methods/utils/src/pg_gp/madlib_keras.c
+++ methods/utils/src/pg_gp/madlib_keras.c
@@ -16,8 +16,8 @@ PG_FUNCTION_INFO_V1(dlc_madlib_keras_fit);
  */
 Datum
 dlc_madlib_keras_fit(PG_FUNCTION_ARGS) {
-    bytea *pg_xdata           = PG_GETARG_BYTEA_P(0);
-    Oid     xdata_typ          = get_fn_expr_argtype(fcinfo->flinfo, 0);
+    bytea *pg_xdata           = PG_GETARG_BYTEA_P(1);
+    Oid     xdata_typ          = get_fn_expr_argtype(fcinfo->flinfo, 1);
     uint32_t rv = -1;
     Datum retval;
 	char *xdata = NULL;
@@ -28,17 +28,20 @@ dlc_madlib_keras_fit(PG_FUNCTION_ARGS) {
 
     xdata = VARDATA(pg_xdata);
 
-    result = palloc(x_size);
-//    memcpy(result, xdata, 50);
+//    result = palloc(x_size + 2);
+//    memcpy(result, xdata, x_size);
+//    result[x_size - 2] = "h";
+//    result[x_size - 1] = "i";
+    result = xdata;
 
 	if (result == NULL) {
 		elog(WARNING, "result PyObject was NULL ptr on return!");
 	} else {
-        length = 50;
+        length = x_size;
 	}
 
     retval = Int32GetDatum(length);
 
-    pfree(result);
+//    pfree(result);
     PG_RETURN_INT32(retval);
 }
