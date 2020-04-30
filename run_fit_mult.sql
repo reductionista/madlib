8a20018c393c5d287a2827866d675001a32571ea
Domino Valdano <dvaldano@pivotal.io>
2020-04-30 07:14:26 +0000

    Add suffix to mst_table (_places10 + _places100)

--- run_fit_mult.sql
+++ run_fit_mult.sql
@@ -1,10 +1,11 @@
 SELECT version() from gp_dist_random('gp_id');
 SELECT madlib.version() from gp_dist_random('gp_id');
+set search_path=uda_output, gpdb6_input, madlib;
 DROP TABLE if exists places10_train_mult_model, places10_train_mult_model_summary, places10_train_mult_model_info;
 SELECT madlib_keras_fit_multiple_model(
     'places10_train_batched',
     'places10_train_mult_model',
-    'mst_table',
+    'mst_table_places10',
     1,
     TRUE
 );
--- run_fit_mult100.sql
+++ run_fit_mult100.sql
@@ -1,10 +1,11 @@
 SELECT version() from gp_dist_random('gp_id');
 SELECT madlib.version() from gp_dist_random('gp_id');
+set search_path=uda_output, gpdb6_input, madlib;
 DROP TABLE if exists places100_train_mult_model, places100_train_mult_model_summary, places100_train_mult_model_info;
 SELECT madlib_keras_fit_multiple_model(
     'places100_train_batched',
     'places100_train_mult_model',
-    'mst_table',
+    'mst_table_places100',
     1,
     TRUE
 );
