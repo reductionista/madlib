222ab9dcaf5f52cdf2606292eeed23e86020f77d
Domino Valdano <dvaldano@pivotal.io>
2020-04-30 06:30:40 +0000

    Add run_fit_mult100.sql

---
+++ run_fit_mult100.sql
@@ -0,0 +1,10 @@
+SELECT version() from gp_dist_random('gp_id');
+SELECT madlib.version() from gp_dist_random('gp_id');
+DROP TABLE if exists places100_train_mult_model, places100_train_mult_model_summary, places100_train_mult_model_info;
+SELECT madlib_keras_fit_multiple_model(
+    'places100_train_batched',
+    'places100_train_mult_model',
+    'mst_table',
+    1,
+    TRUE
+);
