aa99795aee68fb7457e99e9c1f6a7a97fd7fdcd3
Domino Valdano <dvaldano@pivotal.io>
2020-05-06 03:09:32 +0000

    Add warm start runner

---
+++ run_fit_mult_warmstart.sql
@@ -0,0 +1,13 @@
+set search_path=udf_output, gpdb6_input, madlib;
+SELECT version() from gp_dist_random('gp_id');
+SELECT madlib.version() from gp_dist_random('gp_id');
+SELECT madlib_keras_fit_multiple_model(
+    'places10_train_batched',
+    'places10_train_mult_model',
+    'mst_table_places10',
+    1,
+    TRUE,
+    NULL,  -- validation table
+    NULL,  -- metrics compute freq
+    TRUE -- warm start
+);
