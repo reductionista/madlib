1d8adca81c9178ef15ced880a3f3615cfaa97d59
Domino Valdano <dvaldano@vmware.com>
2020-09-25 17:59:25 -0700

    Add scripts for running fit multiple
     - run_fit_mult.sql
     - run_fit_mult100.sql
     - run_fit_mult_iris.sql
     - run_fit_mult_mnist.sql
     - run_fit_mult_warmstart.sql

---
+++ run_fit_mult.sql
@@ -0,0 +1,10 @@
+SELECT version() from gp_dist_random('gp_id');
+SELECT madlib.version() from gp_dist_random('gp_id');
+DROP TABLE if exists places10_train_mult_model_fromscratch, places10_train_mult_model_fromscratch_summary, places10_train_mult_model_fromscratch_info;
+SELECT madlib_keras_fit_multiple_model(
+    'places10_train_batched',
+    'places10_train_mult_model_fromscratch',
+    'mst_table_places10',
+    1,
+    FALSE -- use_gpus
+);
---
+++ run_fit_mult_iris.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS iris_model, iris_model_summary, iris_model_info;
+SELECT madlib.madlib_keras_fit_multiple_model('iris_data_packed', 'iris_model', 'mst_table', 1, FALSE);
